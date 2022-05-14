d04d01c239ef9f41c7b6b8f0dbccbd234d78a279
Lotosney <sebastiandwojcik@gmail.com>
2022-05-14 12:57:57 +0200

    add brutto to netto calculation

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const netPrice = document.getElementById('net-price');
 const vatRate = document.getElementById('vat-rate');
 const incAmount = document.getElementById('inc-amount');
 const vatAmount = document.getElementById('vat-amount');
-const calcBtn = document.getElementById('calc-btn');
+const bruttoBtn = document.getElementById('brutto-btn');
+const nettoBtn = document.getElementById('netto-btn');
 
 
 function VATInclusiveAmount(price, rate){
@@ -13,37 +14,58 @@ function VATAmount(price, rate){
     let taxAmount = price * (rate / 100)
     return taxAmount.toFixed(2)
 }
+function VATExclusiveAmount(price, rate){
+    let nettoAmount =  price/(1+(rate / 100));
+    return nettoAmount.toFixed(2);
+}
 
-calcBtn.addEventListener('click', function(){
+bruttoBtn.addEventListener('click', function(){
     if(netPrice.value === "" || isNaN(netPrice.value)){
-        // showing red border for invalid input
         netPrice.style.border = "1px solid red";
-
-        // removing red border after 1.5s
         setTimeout(function(){
             netPrice.style.border = "1px solid transparent";
         }, 1500);
 
-        // clearing wrong input
         netPrice.value = "";
     }
 
     if(vatRate.value === "" || isNaN(vatRate.value)){
-        // showing red border for invalid input
         vatRate.style.border = "1px solid red";
 
-        // removing red border after 1.5s
         setTimeout(function(){
             vatRate.style.border = "1px solid transparent";
         }, 1500);
 
-        // clearing wrong input
         vatRate.value = "";
     }
 
-    let finalAmount = VATInclusiveAmount(Number(netPrice.value), Number(vatRate.value));
-    incAmount.value = finalAmount;
+    let bruttoAmount = VATInclusiveAmount(Number(netPrice.value), Number(vatRate.value));
+    incAmount.value = bruttoAmount;
     let taxAmount = VATAmount(Number(netPrice.value), Number(vatRate.value));
     vatAmount.value = taxAmount;
 })
+nettoBtn.addEventListener('click', function(){
+    if(incAmount.value === "" || isNaN(incAmount.value)){
+        incAmount.style.border = "1px solid red";
+        setTimeout(function(){
+            incAmount.style.border = "1px solid transparent";
+        }, 1500);
 
+        incAmount.value = "";
+    }
+
+    if(vatRate.value === "" || isNaN(vatRate.value)){
+        vatRate.style.border = "1px solid red";
+
+        setTimeout(function(){
+            vatRate.style.border = "1px solid transparent";
+        }, 1500);
+
+        vatRate.value = "";
+    }
+
+    let nettoAmount = VATExclusiveAmount(Number(incAmount.value), Number(vatRate.value));
+    netPrice.value = nettoAmount;
+    let taxAmount = VATAmount(Number(incAmount.value), Number(vatRate.value));
+    vatAmount.value = taxAmount;
+})
